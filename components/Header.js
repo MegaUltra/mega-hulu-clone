476ec4813b0863309a371dae0bcfb619624b580c
Mikel-Jon West <dwmyke@gmail.com>
2022-02-23 14:33:41 +0000

    Adjusting menu

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,25 @@
 import Image from "next/image"
+import HeaderItem from './HeaderItem'
+import { CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon } from '@heroicons/react/outline'
 
 
 function Header () {
     return (
-        <header className="">
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
             <Image
             className="object-contain" 
             src='/images/logo-wide-white.png' 
             width={200} 
             height={55} 
             />
+            <div className="flex flex-grow justify-evenly max-w-2xl mt-6">
+                <HeaderItem title='Home' Icon={HomeIcon} />
+                <HeaderItem title='Search' Icon={SearchIcon} />
+                <HeaderItem title='Trending' Icon={LightningBoltIcon} />
+                <HeaderItem title='Collections' Icon={CollectionIcon} />
+                
+            </div>
+            
         </header>
     )
 }
