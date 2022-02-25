b8e509059a1efa9e18cc43ebc57eb6939967970c
Mikel-Jon West <dwmyke@gmail.com>
2022-02-25 09:49:20 +0000

    Needed to correct server side rendering on index page. Solution found here: https://github.com/klaryon/hulu-clone-2.0/blob/main/next.config.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import type { NextPage } from 'next'
+import { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
-import Results from '../components/Results'
-import requests from '../utils/requests'
+import Results from "../components/Results";
+import requests from "../utils/requests";
 import Image from 'next/image'
 
-export default function Home({ results }) {
+const Home: NextPage = ({ results }) => {
   return (
     <div className=''>
       <Head>
@@ -25,18 +25,21 @@ export default function Home({ results }) {
   )
 }
 
+export default Home;
+
+//Server side render
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
 
   const request = await fetch(
-    `https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`
-  ).then(
-    res => res.json()
-  );
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
 
   return {
     props: {
-
-    }
-  }
+      results: request.results,
+    },
+  };
 }
