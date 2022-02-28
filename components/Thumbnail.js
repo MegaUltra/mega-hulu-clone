c2395bac1d1cb5e4161cf4fcdd01d83dfb641098
Mikel-Jon West <dwmyke@gmail.com>
2022-02-28 09:50:05 +0000

    Thumbnail done Todo: change icon to play. Don't think I'll have votes on my videos.

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,3 +1,4 @@
+import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 
 
@@ -5,7 +6,7 @@ function Thumbnail({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
-    <div>
+    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-x-105 hover:z-50">
     <Image
                 layout="responsive"
                 src={
@@ -15,6 +16,19 @@ function Thumbnail({ result }) {
                 height={1080}
                 width={1920}
             />
+
+            <div className="p-2">
+            <h2 className="text-2xl text-white font-semibold transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h2>
+            <p>{result.release_date || result.first_air_date}</p>
+              <p className="text-ellipsis max-w-md mt-2">{result.overview}</p>
+
+              <p className="flex items-center opacity-0 group-hover:opacity-100 mt-1">
+                {result.media_type && `${result.media_type} •`}{" "}
+                
+                <ThumbUpIcon className="h-5 mx-2" /> {result.vot_count}
+              </p>
+              
+            </div>
 
     </div>
   )
