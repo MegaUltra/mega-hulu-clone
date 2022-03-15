fe648a5b918b625136815a8ad94017c2fb6107e0
Mikel-Jon West <dwmyke@gmail.com>
2022-03-15 07:55:15 +0000

    Error handling.

--- pages/netflix.tsx
+++ pages/netflix.tsx
@@ -16,16 +16,20 @@ import { getVideos } from '../lib/videos'
 //Server side render
 
 export async function getServerSideProps() {
-  const shortVideos = await getVideos()
+  const shortVideos = await getVideos("PLA7-QbGRlgE14IHVvNa9vA7vLtPlfbVrl")
+  const featureVideos = await getVideos("PLA7-QbGRlgE1Z6Q8hLj1IKRY1hMkzKizz")
+  const musicVideos = await getVideos("PLA7-QbGRlgE1-AyxcV2G6uU-tioOuvIFO")
+  const promoVideos = await getVideos("PLA7-QbGRlgE3w8DZEXwT6jDrG5PWl_zbr")
+  const liveVideos = await getVideos("PLA7-QbGRlgE3mxPIa0vJPCUv5QJIAh7OH")
   // Fetch data from external API
   // const res = await fetch(`https://.../data`)
   // const data = await res.json()
 
   // Pass data to the page via props
-  return { props: { shortVideos } }
+  return { props: { shortVideos, featureVideos, musicVideos, promoVideos, liveVideos } }
 }
 
-const Netflix: NextPage = ({ shortVideos }) => {
+const Netflix: NextPage = ({ shortVideos, featureVideos, musicVideos, promoVideos, liveVideos }) => {
   return (
     <div className=''>
       <Head>
@@ -46,7 +50,10 @@ const Netflix: NextPage = ({ shortVideos }) => {
      imgUrl='/images/lfd-thumb.jpg'
      /> */}
 <SectionCards title='Short Films' videos={shortVideos} />
-
+<SectionCards title='Feature Films' videos={featureVideos} />
+<SectionCards title='Music Videos' videos={musicVideos} />
+<SectionCards title='Promotional Videos' videos={promoVideos} />
+<SectionCards title='Live Performances' videos={liveVideos} />
 
     </div>
   )
